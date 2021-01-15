c00b108de87f709f6496551251670701d54a73ed
arctheowl <ldevlin95@hotmail.co.uk>
2021-01-15 22:42:55 +0000

    Adding js server folder

---
+++ Client/go.mod
@@ -0,0 +1,13 @@
+module github.com/arctheowl/Dashboard-client
+
+go 1.15
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.1.17 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	golang.org/x/sys v0.0.0-20210113181707-4bcb84eeeb78 // indirect
+	golang.org/x/text v0.3.5 // indirect
+)
---
+++ Server/build/precache-manifest.7747a6634a93d8d01a5b4d8357aae556.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23217ccd2822a515bd9abb48842f74da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06fccaa0486e0a69dfc0",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "5f7eb8b347a9792d65c0",
+    "url": "/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "06fccaa0486e0a69dfc0",
+    "url": "/static/js/2.53be65f1.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/static/js/2.53be65f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f7eb8b347a9792d65c0",
+    "url": "/static/js/main.1988fb05.chunk.js"
+  },
+  {
+    "revision": "39ef469c1da73d1121d81d2eb5f649f8",
+    "url": "/static/js/main.1988fb05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f104b0f85c1f5221f2",
+    "url": "/static/js/runtime-main.3435af1b.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
